f8f1ab55484ff687930bf3317e154a4779af9912
andreucastano01 <andreu.castano01@estudiant.upf.edu>
2022-02-12 22:47:34 +0100

    La camara se mueve con el raton

--- The covid game/Assets/CameraController.cs
+++ The covid game/Assets/CameraController.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;        //Public variable to store a reference to the player game object
+    public Transform player;       //Public variable to store a reference to the player game object
     private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+    public float turnSpeed = 4.0f;
 
     // Use this for initialization
     void Start()
@@ -13,23 +14,12 @@ public class CameraController : MonoBehaviour
         offset = transform.position - player.transform.position;
     }
 
-    /*void Update()
-    {
-	if (Input.GetKey(KeyCode.LeftArrow))
-	{
-		transform.Rotate(new Vector3(0, -40f, 0) * Time.deltaTime);
-	}
-	if (Input.GetKey(KeyCode.RightArrow))
-	{
-		transform.Rotate(new Vector3(0, 40f, 0) * Time.deltaTime);
-		tranform.position += (0, 40f, 0) * speed * Time.deltaTime; 
-	}
-    }*/
-
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+	offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
         transform.position = player.transform.position + offset;
+        transform.LookAt(player.position);
     }
 }
