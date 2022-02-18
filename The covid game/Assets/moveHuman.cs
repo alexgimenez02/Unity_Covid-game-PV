a5d009c58c992f7479ff50990b1075a95dd03e41
andreucastano01 <andreu.castano01@estudiant.upf.edu>
2022-02-18 14:34:07 +0100

    Colisiones
    Colisioname esta Wenjie

--- The covid game/Assets/moveHuman.cs
+++ The covid game/Assets/moveHuman.cs
@@ -15,15 +15,15 @@ public class moveHuman : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKey(KeyCode.UpArrow)  || Input.GetKey(KeyCode.W))
+		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
 		{
 			transform.position += -transform.right * speed * Time.deltaTime;
 		}
-		if (Input.GetKey(KeyCode.DownArrow)  || Input.GetKey(KeyCode.S))
+		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
 		{
 			transform.position += transform.right * speed * Time.deltaTime;
 		}
-		if (Input.GetKey(KeyCode.LeftArrow)  || Input.GetKey(KeyCode.A))
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
 		{
 			transform.Rotate(new Vector3(0, -40f, 0) * Time.deltaTime);
 		}
