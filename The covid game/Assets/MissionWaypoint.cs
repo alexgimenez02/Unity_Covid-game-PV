edf844fa716b28633d4d4cb6ec284856ab367138
Dan kaws <daniela.cibotaru01@estudiant.upf.edu>
2022-03-01 16:20:26 +0100

    waypoint y tres misiones para dia 1

--- The covid game/Assets/MissionWaypoint.cs
+++ The covid game/Assets/MissionWaypoint.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class MissionWaypoint : MonoBehaviour
 {
     public RawImage img;
-    // The target (location, enemy, etc..)
+    // Target locations for day one demo
     public Transform target;
+    public Transform target2;
+    public Transform target3;
+    public int goalsAchived;
 
     public Text text;
+    public int distance;
+    public Vector2 pos;
+    private GUIStyle currentStyle;
+    public bool showmessage;
 
 
     // Start is called before the first frame update
@@ -17,46 +25,146 @@ public class MissionWaypoint : MonoBehaviour
     {
        
         text.text = "********************************************************";
+        text.color = Color.white;
+        goalsAchived = 0;
+        showmessage = false;
+
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //guardamos los margenes de la pantalla que vemos
-        float minX = img.GetPixelAdjustedRect().width / 2;
-        float maxX = Screen.width - minX;
-        float minY = img.GetPixelAdjustedRect().height / 2;
-        float maxY = Screen.height - minY;
-
-        //var temporal para guardar de 3d a coordenadas 2d
-        Vector2 pos = Camera.main.WorldToScreenPoint(target.position);
-
-        //Check if the target is behind us, to only show the icon once the target is in front
-        if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
+        if (goalsAchived < 3)
         {
-            // Check if the target is on the left side of the screen
-            if (pos.x < Screen.width / 2)
+            //guardamos los margenes de la pantalla que vemos
+            float minX = img.GetPixelAdjustedRect().width / 2;
+            float maxX = Screen.width - minX;
+            float minY = img.GetPixelAdjustedRect().height / 2;
+            float maxY = Screen.height - minY;
+
+            //var temporal para guardar de 3d a coordenadas 2d
+            pos = Camera.main.WorldToScreenPoint(target.position);
+
+            //Check if the target is behind us, to only show the icon once the target is in front
+            if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
             {
-                // Place it on the right (Since it's behind the player, it's the opposite)
-                pos.x = maxX;
+                // Check if the target is on the left side of the screen
+                if (pos.x < Screen.width / 2)
+                {
+                    // Place it on the right (Since it's behind the player, it's the opposite)
+                    pos.x = maxX;
+                }
+                else
+                {
+                    // Place it on the left side
+                    pos.x = minX;
+                }
             }
-            else
+
+            //limitamos a que la flecha solo pueda estar dentro de la pantalla para verla en todo momento
+            pos.x = Mathf.Clamp(pos.x, minX, maxX);
+            pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+            // Update the marker's position
+            img.transform.position = pos;
+            //write meters to arrive to target
+            distance = (int)Vector3.Distance(target.position, transform.position);
+            text.text = distance.ToString() + "m";
+
+            //player reached target 3
+            if (distance <= 11 && goalsAchived == 2)
             {
-                // Place it on the left side
-                pos.x = minX;
+                goalsAchived = 3;
+                showmessage = true;
+
             }
+
+            //player reached target 2
+            if (distance <= 11 && goalsAchived == 1)
+            {
+                //change target to target 2
+                target = target3;
+                //change goals achieved to 1;
+                goalsAchived = 2;
+                showmessage = true;
+            }
+
+            //player reached target 1
+            if (distance <= 11 && goalsAchived == 0)
+            {
+                //change target to target 2
+                target = target2;
+                //change goals achieved to 1;
+                goalsAchived = 1;
+                showmessage = true;
+            }
+
+
+        }
+        else
+        {
+            img.enabled = false;
         }
 
-        //limitamos a que la flecha solo pueda estar dentro de la pantalla para verla en todo momento
-        pos.x = Mathf.Clamp(pos.x, minX, maxX);
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
-
-        // Update the marker's position
-        img.transform.position = pos;
-        //Debug.Log(((int)Vector3.Distance(target.position, transform.position)).ToString() + "m");
-        text.color = Color.white;
-        text.text = ((int)Vector3.Distance(target.position, transform.position)).ToString() + "m";
-       
     }
+
+    void OnGUI(){
+        int windowWidth = 200;
+        int windowHeight = 200;
+        int x = (Screen.width - windowWidth) / 2;
+        int y = (Screen.height - windowWidth) / 2;
+
+        if (showmessage)
+        {
+            if (GUILayout.Button("CLOSE POPUP"))
+                showmessage = false;
+        }
+        
+
+
+        if (showmessage)
+        {
+            //create a texture for background
+            currentStyle = new GUIStyle(GUI.skin.box);
+            currentStyle.normal.background = MakeTex(2, 2, Color.black);
+
+            if(goalsAchived == 1)
+            {
+                GUI.Label(new Rect(x, y, windowWidth, windowHeight), "Package 1 delivered!! \r\n You receive DEATH \r\n deliver the next package", currentStyle);
+
+            }
+            if (goalsAchived == 2)
+            {
+                GUI.Label(new Rect(x, y, windowWidth, windowHeight), "Package 2 delivered!! \r\n Was just jocking lol deadass  \r\n You receive 50€ \r\n deliver the next package", currentStyle);
+
+            }
+            if (goalsAchived == 3)
+            {
+                GUI.Label(new Rect(x, y, windowWidth, windowHeight), "Package 3 delivered!! \r\n You got COVID \r\n enjoy your 10€ and the trip to ER ", currentStyle);
+
+            }
+
+        }
+            
+        
+    }
+    
+
+    
+    Texture2D MakeTex(int width, int height, Color col)
+    {
+        Color[] pix = new Color[width * height];
+        for (int i = 0; i < pix.Length; ++i)
+        {
+            pix[i] = col;
+        }
+        Texture2D result = new Texture2D(width, height);
+        result.SetPixels(pix);
+        result.Apply();
+        return result;
+    }
+
 }
+
